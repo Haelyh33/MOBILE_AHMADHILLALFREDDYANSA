02592cf55110807d4306d4a3d658a087d2643770
MOBILE_AHMAD HILLAL FREDDYANSA <2212110033@mhs.uqgresik.ac.id>
2024-11-14 23:38:23 +0700

    Update Screen3.kt

--- Screen3.kt
+++ Screen3.kt
@@ -1,11 +1,22 @@
 package com.example.portfolioapp.ui.screens
 
-import androidx.compose.foundation.layout.*
-import androidx.compose.material3.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.navigation.compose.rememberNavController
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun Screen3() {
     Scaffold(
@@ -14,9 +25,11 @@ fun Screen3() {
                 title = { Text(text = "About") }
             )
         }
-    )
-    {
-        Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+    ) { padding ->
+        Column(modifier = Modifier
+            .fillMaxSize()
+            .padding(padding)
+            .padding(16.dp)) {
             Text(text = "About Me", style = MaterialTheme.typography.headlineMedium)
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -29,5 +42,10 @@ fun Screen3() {
             Spacer(modifier = Modifier.height(8.dp))
             Text(text = "Major: Computer Science", style = MaterialTheme.typography.bodyLarge)
         }
-    }
+        }
+}
+@Preview(showBackground = true)
+@Composable
+fun Screen3Preview() {
+    Screen3()
 }
